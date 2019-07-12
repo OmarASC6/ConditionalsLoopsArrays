2800323de149006426252976ef62e72bba3ecaf1
OmarASC6 <omartarekfawzy@gmail.com>
2019-07-12 13:40:33 -0400

    first commit

---
+++ omar/Week0/Day4/coding-demo/coding-demo.js
@@ -0,0 +1,18 @@
+const rawText =
+"Judge connected on the first pitch from Ryan Stanek (0-2) to give him nine career multihomer games";
+
+// function singleArray () { 
+
+// const result = []
+
+const stringlower = rawText
+rawText.toLowerCase();
+console.log ();
+
+// for (let i=0 , i< string.length; i++){
+//     const char = string[i]
+// }
+// if (charisanumber){replace by""}
+
+
+// }
